cf0c9c73d5e5443dae9d616b79f61c55cfd97750
Peter Steneteg <peter@steneteg.se>
2018-12-14 15:54:11 +0100

    Jenkins: refactor

--- tools/jenkins/util.groovy
+++ tools/jenkins/util.groovy
@@ -18,4 +18,24 @@ def cmd(stageName, dirName, env = [], fun) {
     }
 }
 
+def defaultProperties() {
+    return [
+            parameters([
+                booleanParam(
+                    defaultValue: false, 
+                    description: 'Do a clean build', 
+                    name: 'Clean Build'
+                ),
+                choice(
+                    choices: "Release\nDebug\nMinSizeRel\nRelWithDebInfo\n", // The first will be default
+                    description: 'Select build configuration', 
+                    name: 'Build Type'
+                )
+            ]),
+            pipelineTriggers([
+                [$class: 'GitHubPushTrigger']
+            ])
+        ]
+}
+
 return this
